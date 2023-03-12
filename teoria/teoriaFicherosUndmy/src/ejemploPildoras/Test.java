fafe9ea7fb0145a9d8b095f0b2b342109f5c3a80
acastineiraduran <acastineiraduran@danielcastelao.com>
2023-03-12 13:12:15 +0100

    fich

---
+++ teoria/teoriaFicherosUndmy/src/ejemploPildoras/Test.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejemploPildoras;
+
+import ejemploPildoras.Lectura;
+
+/**
+ *
+ * @author casty
+ */
+public class Test {
+    public static void main(String[] args) {
+        Lectura accediendo = new Lectura();
+        accediendo.leerFichero();
+    }
+}
